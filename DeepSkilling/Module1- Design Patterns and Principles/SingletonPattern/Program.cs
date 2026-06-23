37483cf827c3d7f5c53ba453335f11c93fd0f331
SnehaPriya77 <sneha.valmiki77@gmail.com>
2026-06-23 18:12:36 +0530

    Add files via upload

---
+++ DeepSkilling/Module1- Design Patterns and Principles/SingletonPattern/Program.cs
@@ -0,0 +1,8 @@
+﻿Logger logger1 = Logger.GetInstance();
+Logger logger2 = Logger.GetInstance();
+
+logger1.Log("Application Started");
+
+Console.WriteLine(
+    ReferenceEquals(logger1, logger2)
+);
