646fe670a0bc716243355ce3ab7cbf818d0e19e9
SnehaPriya77 <sneha.valmiki77@gmail.com>
2026-07-02 10:03:53 +0530

    Add files via upload

---
+++ DeepSkilling/Module 4- NUnit and Moq/PlayerManager.Tests/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+using PlayersManagerLib;
+
+namespace PlayerManager.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        private Mock<IPlayerMapper> mockPlayerMapper;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            mockPlayerMapper = new Mock<IPlayerMapper>();
+
+            mockPlayerMapper
+                .Setup(x => x.IsPlayerNameExistsInDb(It.IsAny<string>()))
+                .Returns(false);
+
+            mockPlayerMapper
+                .Setup(x => x.AddNewPlayerIntoDb(It.IsAny<string>()));
+        }
+
+        [TestCase]
+        public void RegisterNewPlayer_ShouldCreatePlayer()
+        {
+            Player player = Player.RegisterNewPlayer("Virat", mockPlayerMapper.Object);
+
+            Assert.That(player, Is.Not.Null);
+            Assert.That(player.Name, Is.EqualTo("Virat"));
+            Assert.That(player.Age, Is.EqualTo(23));
+            Assert.That(player.Country, Is.EqualTo("India"));
+            Assert.That(player.NoOfMatches, Is.EqualTo(30));
+        }
+
+        [TestCase]
+        public void RegisterNewPlayer_WithEmptyName_ShouldThrowException()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Player.RegisterNewPlayer("", mockPlayerMapper.Object);
+            });
+        }
+    }
+}
---
+++ DeepSkilling/Module 4- NUnit and Moq/PlayersManagerLib/PlayerMapper.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.SqlClient;
+
+namespace PlayersManagerLib
+{
+    public class PlayerMapper : IPlayerMapper
+    {
+        private readonly string _connectionString =
+            "Data Source=(local);Initial Catalog=GameDB;Integrated Security=True";
+
+        public bool IsPlayerNameExistsInDb(string name)
+        {
+            // Dummy implementation for the exercise.
+            return false;
+        }
+
+        public void AddNewPlayerIntoDb(string name)
+        {
+            // Dummy implementation for the exercise.
+        }
+    }
+}
